1e14d452fac8463d10a707107945fdee05e6b58e
Alex <alexcorreas1994@gmail.com>
2021-05-19 20:13:27 -0300

    Exercicios do bloco 4 dia 1

---
+++ bloco_4/dia_1/exerb4d1.js
@@ -0,0 +1,164 @@
+/*const name = "Alex";
+const birthCity = "Curitiba";
+birthCity = "Joacaba";
+let birthYear = 1994;
+birthYear = 2030;
+
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
+console.log(birthYear);*/
+
+/*let patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientAge);
+console.log(typeof patientId);*/
+
+/*let base = 5;
+let altura = 8;
+let area = base * altura;
+let perimetro = base * 2 + altura * 2;
+
+console.log(area);
+console.log(perimetro);*/
+
+// let notaCandidato = 61;
+
+// if(notaCandidato >=80 && notaCandidato<=100){
+//     console.log("Parabéns, você foi aprovada(o)!")
+// }
+// else if (notaCandidato < 80 && notaCandidato >= 60){
+//     console.log("Você está na nossa lista de espera")
+// }
+// else if (notaCandidato < 60){
+//     console.log("Você foi reprovada(o)")
+// }
+
+// let estadoPessoa = 'xablau';
+
+// switch(estadoPessoa){
+//     case 'aprovada':
+//         console.log("Essa pessoa está aprovada!!");
+//         break;
+//     case 'lista' :
+//         console.log("Essa pessoa está na lista de espera");
+//         break;
+//     case 'reprovada' :
+//         console.log("Essa está reprovada");
+//         break;
+//     default:
+//         console.log("não se aplica");    
+// }
+
+// exercicio 1
+// let a = 81;
+// let b = 72;
+
+// //adição
+// sum = a+b;
+// console.log("A soma é:", + sum);
+
+// //subtração
+// sub = a - b;
+// console.log("A subtração é:", + sub);
+
+// //multiplicação
+// multi = a * b;
+// console.log("A multiplicação é", + multi);
+
+// //divisão
+// div = a / b;
+// console.log("A divisão é", + div);
+
+// //mod
+// mod = a % b;
+// console.log("o MOD é:", + mod);
+
+//exercicio 2
+
+// let a = 4;
+// let b = 3;
+
+// if(a>b){
+//     console.log(a);
+// }
+// else{
+//     console.log(b);
+// }
+
+// exercicio 3
+// let a = 8;
+// let b = 10;
+// let c = 7;
+
+// if (a>b && a> c){
+//     console.log(a);
+// }
+// else if(b>a && b > c){
+//     console.log(b);
+// }
+// else{
+//     console.log(c);
+// }
+
+//exercicio 4
+
+// let a = 1;
+
+// if(a>0){
+//     console.log("Positivo");
+// }
+// else if(a<0){
+//     console.log("Negativo");
+// }
+// else{
+//     console.log("Zero");
+// }
+
+// exercicio 5
+
+// let ang1 = 'xablau';
+// let ang2 = "xablau";
+// let ang3 = 'xablau';
+
+// if(ang1 + ang2 + ang3 == 180){
+//     console.log(true);
+// }
+// else if (ang1 + ang2 + ang3 > 180 ){
+//     console.log(false);
+// }
+// else{
+//     console.log('ERROR!!!!')
+// }
+
+// exercicio 6
+
+// let peca = 'Cavalo';
+
+// switch(peca){
+//     case  'peao' :
+//         console.log('reto ->',peca.toLowerCase());
+//         break;
+//     case 'bispo':
+//         console.log('diagonal ->',peca.toLowerCase());
+//         break;
+    
+//     case 'rainha' :
+//         console.log('todas as direções quantas casas quiser ->', peca.toLowerCase());
+//         break;
+    
+//     case 'rei' :
+//         console.log('todas as direções uma vez só ->', peca.toLowerCase());
+//         break;
+//     case 'cavalo' :
+//         peca.toLowerCase();
+//         console.log('Em L ->', peca);
+//         break;
+// }
+
