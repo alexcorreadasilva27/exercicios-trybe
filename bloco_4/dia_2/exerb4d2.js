c6b6fd0b03cc5cbecd5d4482af9e40462c287df9
Alex <alexcorreas1994@gmail.com>
2021-05-20 20:02:07 -0300

    Exercicios resolvidos ate o 4

---
+++ bloco_4/dia_2/exerb4d2.js
@@ -0,0 +1,60 @@
+// exercicio array e for
+
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+// for (let index = 0; index < groceryList.length; index +=1) {
+//   console.log(groceryList[index]);
+// }
+
+// for e of
+// let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+// for(let name of names)
+// console.log(name);
+
+ 
+ 
+//1.Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for(let index = 0; index<numbers.length; index+=1){
+
+//  console.log(numbers[index]);}
+
+//2.Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let result = 0;
+// for(let index = 0; index<numbers.length; index+=1){
+// result+=numbers[index];
+// }
+// console.log(result);
+
+//3.Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let sum = 0;
+// for(let index = 0; index<numbers.length; index+=1){
+// sum+=numbers[index];
+// }
+// average = sum / numbers.length
+// console.log(average);
+
+//4.Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+// let numbers = [5, 9, 3, 19, 70, 8, 10, 2, 35, 27];
+// let sum = 0;
+// for(let index = 0; index<numbers.length; index+=1){
+// sum+=numbers[index];
+// }
+// average = sum / numbers.length
+// console.log(average);
+
+// if(average > 20){
+//     console.log('valor maior que 20');
+// }
+// else{
+//     console.log('valor menor que 20');
+// }
+
+//5. Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+
+let numbers = [5, 9, 3, 19, 70, 8, 10, 2, 35, 27];
