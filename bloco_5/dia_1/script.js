a6f7c54be44e35905371eac31f4d768a806707ec
Alex <alexcorreas1994@gmail.com>
2021-05-26 21:12:44 -0300

    Exercicios feitos

---
+++ bloco_5/dia_1/script.js
@@ -0,0 +1,29 @@
+let emergencyTasks = document.getElementsByClassName("emergency-tasks")[0];
+emergencyTasks.style.background = "pink";
+emergencyTasks.style.color = "white"
+
+let emergencyTask = document.querySelector("h3");
+emergencyTask.style.background = "purple";
+
+let emergencyTask1 = document.querySelectorAll("h3")[1];
+emergencyTask1.style.background = "purple";
+
+let noEmergencyTasks = document.getElementsByClassName("no-emergency-tasks")[0];
+noEmergencyTasks.style.background = "yellow";
+noEmergencyTasks.style.color = "white"
+
+let noEmergencyTask = document.querySelectorAll("h3")[2];
+noEmergencyTask.style.background = "black";
+
+let noEmergencyTask1 = document.querySelectorAll("h3")[3];
+noEmergencyTask1.style.background = "black";
+
+let header = document.querySelector("h1");
+header.style.background = "green";
+
+let footer = document.querySelector("footer");
+footer.style.background = "black";
+footer.style.color = "white";
+
+let body = document.querySelector("body")
+body.style.background = "red";
