2a6368cd35123504f5dbe6d31fdb91f507fecb83
Alex <alexcorreas1994@gmail.com>
2021-05-28 16:22:50 -0300

    EXercicios do Conteúdo

---
+++ bloco_5/dia_3/main.js
@@ -0,0 +1,93 @@
+const divUm = document.getElementById('divUm');
+const divDois = document.getElementById('divDois');
+const divTres = document.getElementById('divTres');
+const input = document.getElementById('input');
+const myWebpage = document.getElementById('mySpotrybefy');
+
+/*
+ Copie esse arquivo e edite apenas ele;
+ Crie uma função que adicione a classe 'tech' ao elemento selecionado;
+1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?*/
+
+
+function oneTech(evento){
+    let techElemento = document.querySelector('.tech');
+    techElemento.classList.remove('tech');
+    evento.target.classList.add('tech');
+    input.value = '';
+
+}
+divUm.addEventListener('click', oneTech);
+divDois.addEventListener('click', oneTech);
+divTres.addEventListener('click', oneTech);
+
+
+ /*Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+com a classe 'tech';*/
+ 
+input.addEventListener('input', (evento) => {
+    let techElemento = document.querySelector('.tech');
+    techElemento.innerText = evento.target.value;
+});
+
+
+
+/* Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+redirecione para alguma página;
+1. Que tal redirecionar para seu portifólio?*/
+
+myWebpage.addEventListener('dblclick', () =>{
+    window.location.replace('https://blog.betrybe.com/');
+});
+
+
+ /*Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+a cor do mesmo;*/
+
+myWebpage.addEventListener('mouseover', (evento) =>
+{
+    evento.target.style.color = 'red';
+});
+myWebpage.addEventListener('mouseout', (evento) =>{
+    evento.target.style.color = 'unset';
+});
+/*Segue abaixo um exemplo do uso de event.target:
+*/
+
+function resetText(event) {
+  // O Event é passado como um parâmetro para a função.
+  event.target.innerText = 'Opção reiniciada';
+  // O event possui várias propriedades, porém a mais usada é o event.target,
+  // que retorna o objeto que disparou o evento.
+}
+
+divUm.addEventListener('dblclick', resetText);
+
+
+// Não precisa passar o parâmetro dentro do addEventListener. O próprio
+// navegador fará esse trabalho por você, não é legal? Desse jeito, o
+// event.target na nossa função retornará o objeto 'divUm'.
+divUm.addEventListener('mouseover', (evento) => {
+    evento.target.style.color = 'red';
+});
+divUm.addEventListener('mouseout', (evento) => {
+    evento.target.style.color = 'unset';
+});
+
+divDois.addEventListener('dblclick', resetText);
+
+divDois.addEventListener('mouseover', (evento) => {
+    evento.target.style.color = 'red';
+});
+divDois.addEventListener('mouseout', (evento) => {
+    evento.target.style.color = 'unset';
+});
+
+divTres.addEventListener('dblclick', resetText);
+
+divTres.addEventListener('mouseover', (evento) => {
+    evento.target.style.color = 'red';
+});
+divTres.addEventListener('mouseout', (evento) => {
+    evento.target.style.color = 'unset';
+});
