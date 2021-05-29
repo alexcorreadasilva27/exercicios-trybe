cb31e3f13346c74a2ff936dad05ba6c2430744c8
Alex <alexcorreas1994@gmail.com>
2021-05-29 02:03:25 -0300

    Calendario em progresso

--- bloco_5/dia_3/script2.js
+++ bloco_5/dia_3/script2.js
@@ -15,25 +15,27 @@ function createDaysOfTheWeek() {
   
   // Escreva seu código abaixo.
 
-  let mayDaysList = [25,26,27,28,29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  let mayDaysList = [25,26,27,28,29,30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
   function criandoOMes(){
       let pegandoListaDosDias = document.querySelector('#days');
       for(index = 0; index < mayDaysList.length; index += 1){
           let day = mayDaysList[index]
           let dayItem = document.createElement('li');
-          if(day === 1){
+          if(day === mayDaysList[6]){
               dayItem.className = 'holiday';
               dayItem.innerHTML = day;
               pegandoListaDosDias.appendChild(dayItem);
-          }else if(day === 7 || day === 14 || day === 21 || day === 28){
+          }else if(day === mayDaysList[5]  || day === mayDaysList[12]|| day === mayDaysList[19]||  day === mayDaysList[26] || day === mayDaysList[33]){
               dayItem.className = 'friday';
               dayItem.innerHTML = day;
-              pegandoListaDosDias.appendChild(dayItem);  
-          }else{
+              pegandoListaDosDias.appendChild(dayItem);
+            }
+          
+          else{
               dayItem.innerHTML = day;
               dayItem.className = 'day';
-              pegandoListaDosDias.appendChild(dayItem);
+              pegandoListaDosDias.appendChild(dayItem); 
           }
       };
   };
@@ -66,28 +68,42 @@ function AnimacaoDosFeriados(){
     })
 };
 
-// function displayHolidays() {
-//     let getHolidayButton = document.querySelector('#btn-holiday');
-//     let getHolidays = document.querySelectorAll('.holiday')
-//     let backgroundColor = 'rgb(238,238,238)';
-//     let setNewColor = 'white';
-  
-//     getHolidayButton.addEventListener('click', function() {
-//       for (let index = 0; index < getHolidays.length; index += 1) {
-//         if (getHolidays[index].style.backgroundColor === setNewColor) {
-//           getHolidays[index].style.backgroundColor = backgroundColor;
-//         } else {
-//           getHolidays[index].style.backgroundColor = setNewColor;
-//         }
-//       }
-//     })
-//   };
-  
- 
+function criandoBotaoSextaFeira(buttoName){
+    let buttonContainer = document.querySelector('.buttons-container');
+    let newButton = document.createElement('button');
+    let buttonNewID = 'btn-friday';
 
+    newButton.innerHTML = buttoName;
+    newButton.id = buttonNewID;
+    buttonContainer.appendChild(newButton);
+
+}
+
+function AnimacaodasSextasFeiras(sextaFeirasArray){
+    let pegandooBotaoDeSexta = document.querySelector('#btn-friday');
+    let sextasFeiras = document.getElementsByClassName('friday');
+    let NovoTextoParaSexta = 'SEXTOU o/';
+
+    pegandooBotaoDeSexta.addEventListener('click', function(){
+        for(let index = 0; index < sextasFeiras.length; index += 1){
+            if(sextasFeiras[index].innerHTML !== NovoTextoParaSexta){
+                sextasFeiras[index].innerHTML = NovoTextoParaSexta;
+            }else{
+                sextasFeiras[index].innerHTML = sextaFeirasArray[index];
+            }
+        }
+    })
+};
+
+
+
+
+
+
+let SextasDeMaio = [30,7,14,21,28]
 criandoOMes();
 criandoBotaoDosFeriados('Feriados'); 
 AnimacaoDosFeriados();
-  
-   
+criandoBotaoSextaFeira("Sexta-Feira");
+AnimacaodasSextasFeiras(SextasDeMaio);   
   
